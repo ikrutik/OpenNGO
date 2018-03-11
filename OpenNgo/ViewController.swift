f2d7f23c18552d47ec58f3a2710e7b5716eb3eb8
ikrutik <serik.krutikov@nstr.space>
2018-03-11 15:52:27 +0300

    Initial Commit

---
+++ OpenNgo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenNgo
+//
+//  Created by Sergey Krutikov on 11.03.2018.
+//  Copyright © 2018 Sergey Krutikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
