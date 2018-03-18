f20040142ef437b5233e8b949a86cda2dbc6fb71
ikrutik <serik.krutikov@nstr.space>
2018-03-18 15:31:48 +0300

    Added base UI Cells

---
+++ OpenNgo/Classes/Modules/DetailsOrganization/View/ContractCell/ContractTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ContractTableViewCell.swift
+//  OpenNgo
+//
+//  Created by Sergey Krutikov on 16.03.2018.
+//  Copyright © 2018 Sergey Krutikov. All rights reserved.
+//
+
+import UIKit
+
+class ContractTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
